50a0cd2d3d85d7dd48d80fd3323cbcbc7b894af0
coobeet <coobeet@gmail.com>
2023-03-22 23:10:33 +0800

    feat: finish http-hello project

--- project-http-hello/main.go
+++ project-http-hello/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 func main() {
+	// Your solution goes here. Good luck!
+	http.HandleFunc("/hello", Hello)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	fmt.Fprint(w, "Hello, "+name)
 	// Your solution goes here. Good luck!
 }
